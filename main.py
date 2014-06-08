eafd81f2cbba3d3b85e6f5fae641d93fed9cd925
chris <chris.carty@gmail.com>
2014-06-08 10:12:21 -0400

    added accounts file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from properties import propertyData, insertProperty, workit
 
+f = open('accounts.txt')
+lines = f.readlines()
+
+user = lines[0].strip()
+password = lines[1].strip()
+database = lines[2].strip()
+host = lines[3].strip()
+
+f.close()
+
 #make connection to database and get cursor
-cur, con = propertyData('localhost', 'root', "1346", 'cityProperties')
+cur, con = propertyData(host, user, password , database)
 
 #for fun lets insert a property
 #insertProperty('5161286', '32', 'Wolfgang', 'Drive', 0, 0, cur)
